01f7de710c7c0ec4ce80db018008d7ed1a3b7319
aakashDeveloper <ahanda205@gmail.com>
2019-09-12 02:49:24 +0100

    a

---
+++ UnitTesting/src/app/first.spec.ts
@@ -0,0 +1,21 @@
+describe('my first test', () => {
+    let out;
+
+    beforeEach(() => {
+        out = {};
+    });
+
+    it('should be true if true', () => {
+        // arrange
+        out.a = false;
+
+        // act
+        out.a = true;
+
+        // assert
+        expect(out.a).toBe(true);
+    });
+
+
+});
+
